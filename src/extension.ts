af8eefef258b9c75dda1493cbfb3490038f83bb8
James McClung <james@themcclungs.net>
2023-12-30 21:05:23 -0500

    extension, package: register copy commands

--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,10 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('jumpselect.jumpRight', () => jumpselect.jump(1, false, showInputBox())));
 	context.subscriptions.push(vscode.commands.registerCommand('jumpselect.jumpSelectLeft', () => jumpselect.jump(-1, true, showInputBox())));
 	context.subscriptions.push(vscode.commands.registerCommand('jumpselect.jumpSelectRight', () => jumpselect.jump(1, true, showInputBox())));
+	context.subscriptions.push(vscode.commands.registerCommand('jumpselect.copyJumpLeft', () => jumpselect.copyJump(-1, false)));
+	context.subscriptions.push(vscode.commands.registerCommand('jumpselect.copyJumpRight', () => jumpselect.copyJump(1, false)));
+	context.subscriptions.push(vscode.commands.registerCommand('jumpselect.copyJumpSelectLeft', () => jumpselect.copyJump(-1, true)));
+	context.subscriptions.push(vscode.commands.registerCommand('jumpselect.copyJumpSelectRight', () => jumpselect.copyJump(1, true)));
 }
 
 function showInputBox(): boolean | undefined {
