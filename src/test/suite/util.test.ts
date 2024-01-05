2ac62e6b330ba5ca2ede94b48f36f754bca73afd
James McClung <james@themcclungs.net>
2024-01-05 15:02:28 -0500

    util (test): skip->dontSkip, +dontMatchWrongWay

--- src/test/suite/util.test.ts
+++ src/test/suite/util.test.ts
@@ -20,11 +20,15 @@ suite('Util Test Suite', () => {
         assert.strictEqual(4, util.getOffsetOfTarget("01234", 2, 1, "4"));
     });
 
-    test('getOffsetOfTarget.skipCharLeft', () => {
-        assert.strictEqual(0, util.getOffsetOfTarget("a12a4", 4, -1, "a"));
+    test('getOffsetOfTarget.dontSkipCharLeft', () => {
+        assert.strictEqual(3, util.getOffsetOfTarget("a12a4", 4, -1, "a"));
     });
 
-    test('getOffsetOfTarget.skipCharRight', () => {
-        assert.strictEqual(3, util.getOffsetOfTarget("a12a4", 0, 1, "a"));
+    test('getOffsetOfTarget.dontSkipCharRight', () => {
+        assert.strictEqual(0, util.getOffsetOfTarget("a12a4", 0, 1, "a"));
+    });
+
+    test('getOffsetOfTarget.dontMatchWrongWay', () => {
+        assert.strictEqual(0, util.getOffsetOfTarget("a12a4", 3, -1, "a"));
     });
 });
